72c205025281c0128d294927eddf1639b8b2037c
Mounika <monikamini659@gmail.com>
2024-08-10 16:16:24 +0530

    javascipt

---
+++ one.js
@@ -0,0 +1,15 @@
+console.log("javascript")
+console.log(90)
+
+    var t=10,
+    t1=20,
+    t2=30
+    function f1(){
+    console.log(t,t1,t2);
+    }
+    f1();
+
+
+    var test1=10;
+    var test2=30;
+    console.log(test1);
---
+++ two.js
@@ -0,0 +1,7 @@
+var  test=100;
+var test1=200;
+function f1(){
+console.log(test);
+console.log(test1);
+}
+f1();
